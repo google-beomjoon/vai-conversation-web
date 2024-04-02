5c01512ddbfb8f26ca7afc33c1fb51ca9559d69b
beomjoon <beomjoon@google.com>
2024-04-02 19:00:35 +0900

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,12 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 export default async (request, context) => {
+	const headers = {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "Content-Type",
+		"Access-Control-Allow-Methods": "GET, POST"
+	}
+
 	try {
 		const userInput = await request.text();
 		const filePath = path.join(__dirname, 'data', `${userInput}-markdown.json`);
 		const data = fs.readFileSync(filePath, 'utf-8');
-		return Response.json(JSON.parse(data));
+		return Response.json(JSON.parse(data), {
+			statusCode: 200,
+			headers: headers
+		});
 	} catch (error) {
-		return Response.json({ error }, { status: 500 });
+		return Response.json({ error: 'Internal Server Error' }, { 
+			status: 500,
+			headers: headers
+		});
 	}
 };
