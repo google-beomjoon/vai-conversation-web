c26fe7623f447fbfecd018b55e9589ae7b3c251f
beomjoon <beomjoon@google.com>
2024-04-02 17:39:55 +0900

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -15,7 +15,18 @@
 export default async (request, context) => {
 	try {
 		const body = await request.json();
-		return Response.json({ resData: body });
+		const url = "https://semiconductor.samsung.com/processor/mobile-processor/exynos-2400/"; // body.url;
+		const response = await fetch(url);
+		const text = await response.text();
+		const parser = new DOMParser();
+        const doc = parser.parseFromString(text, 'text/html');
+
+        var thumbnail = doc.querySelector('meta[name="thumbnail"]')?.content;
+		if (!thumbnail) thumbnail = doc.querySelector('meta[property="og:image"]')?.content;
+        var title = doc.querySelector('meta[property="og:title"]')?.content;
+        var description = doc.querySelector('meta[property="og:description"]')?.content;
+
+		return Response.json({ thumbnail, title, description });
 	} catch (error) {
 		return Response.json({ error }, { status: 500 });
 	}
