e21181e61863b6d63da2b0b872386aec5fe98eaa
beomjoon <beomjoon@google.com>
2024-04-02 17:31:31 +0900

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,12 +1,22 @@
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
+// const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
+
+// export default async (request, context) => {
+//   try {
+//     const response = await fetch(API_ENDPOINT);
+//     const data = await response.json();
+//     return Response.json({ data });
+//   } catch (error) {
+//     console.log(error);
+//     return Response.json({ error: 'Failed fetching data' }, { status: 500 });
+//   }
+// };
+
 
 export default async (request, context) => {
-  try {
-    const response = await fetch(API_ENDPOINT);
-    const data = await response.json();
-    return Response.json({ data });
-  } catch (error) {
-    console.log(error);
-    return Response.json({ error: 'Failed fetching data' }, { status: 500 });
-  }
+	try {
+		const body = request.body;
+		return Response.json({ resData: body });
+	} catch (error) {
+		return Response.json({ error }, { status: 500 });
+	}
 };
