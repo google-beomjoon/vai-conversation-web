6b9b8084d6131cbef667880de70a6c632b66a62f
beomjoon <beomjoon@google.com>
2024-04-02 18:39:28 +0900

    Update sample data

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -10,11 +10,12 @@
 //     return Response.json({ error: 'Failed fetching data' }, { status: 500 });
 //   }
 // };
-
+const fs = require('fs');
+const path = require('path');
 
 export default async (request, context) => {
 	try {
-		const json = fs.readFileSync(require.resolve('/data/scen1-markdown.json'));
+		const json = fs.readFileSync(path.join(__dirname, 'data', 'scen1-markdown.json'));
 		return Response.json({ res: json });
 	} catch (error) {
 		return Response.json({ error }, { status: 500 });
