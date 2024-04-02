f12c7689c8fdd32eae7b20e7480a88f2e8e698cf
beomjoon <beomjoon@google.com>
2024-04-02 18:46:22 +0900

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,23 +1,12 @@
-// const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
-
-// export default async (request, context) => {
-//   try {
-//     const response = await fetch(API_ENDPOINT);
-//     const data = await response.json();
-//     return Response.json({ data });
-//   } catch (error) {
-//     console.log(error);
-//     return Response.json({ error: 'Failed fetching data' }, { status: 500 });
-//   }
-// };
 const fs = require('fs');
 const path = require('path');
 
 export default async (request, context) => {
 	try {
-		const filePath = path.join(__dirname, 'data', 'scen1-markdown.json');
+		const userInput = await request.text();
+		const filePath = path.join(__dirname, 'data', `${userInput}-markdown.json`);
 		const data = fs.readFileSync(filePath, 'utf-8');
-		return Response.json(data);
+		return Response.json(JSON.parse(data));
 	} catch (error) {
 		return Response.json({ error }, { status: 500 });
 	}
